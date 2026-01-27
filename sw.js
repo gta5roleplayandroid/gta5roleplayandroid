4a0b6e9bab8349f49c12d641b2269061d3781656
gta5roleplayandroid <lmore000013@gmail.com>
2026-01-27 17:35:44 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-self.addEventListener('install', e => {
+self.addEventListener("install", e => {
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+self.addEventListener("activate", e => {
+  clients.claim();
 });
